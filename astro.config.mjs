1f79156de83b55c0886f09432ec95c8d17ddb746
Germanldb <germanldb8@gmail.com>
2026-05-04 19:32:16 -0400

    fix: update vercel adapter import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Habilitamos el modo servidor para las APIs
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     react(),
     tailwind()
