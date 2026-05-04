3fe294920ff6e4e32c0ee47fa34901aa41e96880
Germanldb <germanldb8@gmail.com>
2026-05-04 19:31:15 -0400

    fix: add vercel adapter and enable SSR mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [react(), tailwind()]
+  output: 'server', // Habilitamos el modo servidor para las APIs
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
+  integrations: [
+    react(),
+    tailwind()
+  ],
 });
